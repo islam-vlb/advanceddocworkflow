dba5c83640c066c6c0d3b383bcd55b2700a2d16e
islam-vlb <aitmatensalama@gmail.com>
2026-07-06 21:34:46 +0100

    Update business address to El Paso, TX

--- lib/config.ts
+++ lib/config.ts
@@ -4,7 +4,7 @@ export const BUSINESS = {
   phone: "(877) 671-3137",
   email: "support@advanceddocumentworkflow.com",
   descriptor: "advanceddocumentworkfl",
-  address: "4711 34th St. N. Suite F, St. Petersburg, FL 33714",
+  address: "3590 N Zaragoza Road Ste B103 Unit #107, El Paso, TX, 79938",
   processor: "Payarc",
   saas: true,
 };
